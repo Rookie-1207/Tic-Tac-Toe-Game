5fda58f492651331c4db2876ed50861c89d7300b
Aditya Jain <adityajain.jan07@gmail.com>
2026-04-20 08:53:56 +0530

    UC2: Toss and assign symbols

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,22 +1,42 @@
+import java.util.Random;
+
 public class TicTacToe {
+
+    static boolean isPlayer1Turn;
+    static char player1Symbol;
+    static char player2Symbol;
+
     public static void main(String[] args) {
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
 
-        char[][] board = new char[3][3];
+    static void tossAndAssignSymbols() {
+        Random rand = new Random();
+        int toss = rand.nextInt(2); // 0 or 1
 
-   
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
+        if (toss == 0) {
+            isPlayer1Turn = true;
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+        } else {
+            isPlayer1Turn = false;
+            player1Symbol = 'O';
+            player2Symbol = 'X';
+        }
+    }
+
+
+    static void displayTossResult() {
+        System.out.println("=== Toss Result ===");
+
+        if (isPlayer1Turn) {
+            System.out.println("Player 1 starts first!");
+        } else {
+            System.out.println("Player 2 starts first!");
         }
 
-        System.out.println("Tic-Tac-Toe Board:");
-
-        for(int i = 0; i < 3; i++) {
-            for(int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
+        System.out.println("Player 1 symbol: " + player1Symbol);
+        System.out.println("Player 2 symbol: " + player2Symbol);
     }
 }
