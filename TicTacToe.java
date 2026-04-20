abf687678051d2284e9d801559f52c41571c57be
Aditya Jain <adityajain.jan07@gmail.com>
2026-04-20 08:57:19 +0530

    UC3: Accept user slot input

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,42 +1,20 @@
-import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
 
-    static boolean isPlayer1Turn;
-    static char player1Symbol;
-    static char player2Symbol;
-
     public static void main(String[] args) {
-        tossAndAssignSymbols();
-        displayTossResult();
+
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
     }
 
-    static void tossAndAssignSymbols() {
-        Random rand = new Random();
-        int toss = rand.nextInt(2); // 0 or 1
+  
+    static int getUserSlot() {
+        Scanner sc = new Scanner(System.in);
 
-        if (toss == 0) {
-            isPlayer1Turn = true;
-            player1Symbol = 'X';
-            player2Symbol = 'O';
-        } else {
-            isPlayer1Turn = false;
-            player1Symbol = 'O';
-            player2Symbol = 'X';
-        }
-    }
+        System.out.print("Enter slot number (1-9): ");
+        int slot = sc.nextInt();
 
-
-    static void displayTossResult() {
-        System.out.println("=== Toss Result ===");
-
-        if (isPlayer1Turn) {
-            System.out.println("Player 1 starts first!");
-        } else {
-            System.out.println("Player 2 starts first!");
-        }
-
-        System.out.println("Player 1 symbol: " + player1Symbol);
-        System.out.println("Player 2 symbol: " + player2Symbol);
+        return slot;
     }
 }
